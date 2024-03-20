153fd474cf40ce9b77f512c8f4e3744d6f56ae04
abiola <abiola.arobieke@gmail.com>
2024-03-20 05:34:05 -0700

    Add typsScript files to the src folder

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    formElement: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    decriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const templateElementContent = document.importNode(this.templateElement.content, true);
+        this.formElement = templateElementContent.firstElementChild as HTMLFormElement;
+        this.formElement.id = 'user-input';
+
+        this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement;
+        this.decriptionInputElement = this.formElement.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.formElement.querySelector('#people') as HTMLInputElement;
+        this.configure();
+
+        this.attach();
+    }
+
+    private submitHandler(e: Event) {
+        e.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+
+    private configure() {
+        this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+    }
+}
+
+const pr = new ProjectInput();
